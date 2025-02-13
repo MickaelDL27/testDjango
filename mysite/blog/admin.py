726fac650a5ff32ea6f6fdc87d4be7dcb3167983
zendude <mdelembre@gmail.com>
2025-02-13 14:27:51 +0100

    model voetbalspeler toegevoegd

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,3 +4,7 @@ from .models import Voetbalspeler
 
 admin.site.register(Post)
 admin.site.register(Voetbalspeler)
+class VoetbalspelerAdmin(admin.ModelAdmin):
+     list_display = ('naam', 'actuele_voetbalclub', 'auteur', 'datum_invoer', 'datum_laatste_aanpassing')
+     search_fields = ('naam', 'actuele_voetbalclub', 'auteur')
+     list_filter = ('datum_invoer', 'datum_laatste_aanpassing')
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,15 +17,14 @@ class Post(models.Model):
        return self.title
 
 class Voetbalspeler(models.Model):   
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     naam = models.CharField(max_length=200)
     actuele_club = models.CharField(max_length=200)
     datum_invoer = models.DateTimeField(default=timezone.now)
     datum_laatste_aanpassing = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-       self.published_date = timezone.now()
+       self.datum_laatste_aanpassing = timezone.now()
        self.save()
 
     def __str__(self):
