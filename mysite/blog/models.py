a984c77796f20f03dc82d14b9855b19ea3c22c6d
zendude <mdelembre@gmail.com>
2025-02-13 11:35:56 +0100

    Model Voetbalspelers toegevoegd

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,16 +2,30 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):   
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+#class Post(models.Model):   
+#   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#   title = models.CharField(max_length=200)
+#   text = models.TextField()
+#   created_date = models.DateTimeField(default=timezone.now)
+#   published_date = models.DateTimeField(blank=True, null=True)
+
+#   def publish(self):
+#      self.published_date = timezone.now()
+#      self.save()
+
+#   def __str__(self):
+#      return self.title
+
+class Voetbalspelers(models.Model):
+    naam = models.CharField(max_length=255)
+    actuele_club = models.CharField(max_length=255)
+    auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    datum_invoer = models.DateTimeField(auto_now_add=True)
+    datum_laatste_aanpassing = models.DateTimeField(auto_now=True)
 
     def publish(self):
-       self.published_date = timezone.now()
-       self.save()
+        self.datum_invoer = timezone.now()
+        self.save()
 
     def __str__(self):
-       return self.title
+        return self.naam
