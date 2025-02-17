635eb74bf8f73658d22198c80092be4b4b5b236a
zendude <mdelembre@gmail.com>
2025-02-17 11:35:23 +0100

    toevoegen untracked files

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from .models import Post
-from .models import Voetbalspeler
 
 admin.site.register(Post)
-admin.site.register(Voetbalspeler)
-class VoetbalspelerAdmin(admin.ModelAdmin):
-     list_display = ('naam', 'actuele_voetbalclub', 'auteur', 'datum_invoer', 'datum_laatste_aanpassing')
-     search_fields = ('naam', 'actuele_voetbalclub', 'auteur')
-     list_filter = ('datum_invoer', 'datum_laatste_aanpassing')
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,17 +15,3 @@ class Post(models.Model):
 
    def __str__(self):
        return self.title
-
-class Voetbalspeler(models.Model):   
-    auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    naam = models.CharField(max_length=200)
-    actuele_club = models.CharField(max_length=200)
-    datum_invoer = models.DateTimeField(default=timezone.now)
-    datum_laatste_aanpassing = models.DateTimeField(default=timezone.now)
-
-    def publish(self):
-       self.datum_laatste_aanpassing = timezone.now()
-       self.save()
-
-    def __str__(self):
-       return self.naam
