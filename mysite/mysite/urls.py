6ad2f22bd95b3349632ec3fec0b91e933e8ad349
zendude <mdelembre@gmail.com>
2025-02-18 12:00:18 +0100

    HuiswerkL6

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('polls/', include('polls.url')),
+    path('polls/', include('polls.urls')),
 ]
